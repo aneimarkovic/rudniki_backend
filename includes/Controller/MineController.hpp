f63831df1f3e27e08548e072d7270822079af30f
Big Smoke <105986773+Anomi132@users.noreply.github.com>
2025-06-01 21:57:19 +0200

    IR-159 Statistika rudnikov DONE

--- includes/Controller/MineController.hpp
+++ includes/Controller/MineController.hpp
@@ -31,6 +31,7 @@ public:
     static void deleteInfrastructure(const request &request, response &response, Router* r);
     static void deleteMineral(const request &request, response &response, Router* r);
     static void updateMine(const request &request, response &response, Router* r);
+    static void getStatistics(const request& request, response& response, Router* r);
 };
 
 #endif
